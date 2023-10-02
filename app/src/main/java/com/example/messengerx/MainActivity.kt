fb173b5780535bbc6a64a5b6397a882283fc6a4d
Kairkhan Mamai <kajyrhanmamaj@gmail.com>
2023-10-02 21:10:16 +0600

    Finished version

--- app/src/main/java/com/example/messengerx/MainActivity.kt
+++ app/src/main/java/com/example/messengerx/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.messengerx
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.messengerx.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -18,29 +21,53 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var auth: FirebaseAuth
+    lateinit var adapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
-
+        setUpActionBar()
         val database = Firebase.database
         val myRef = database.getReference("messages")
         binding.bSend.setOnClickListener {
-            myRef.setValue(binding.edMessage.text.toString())
+            myRef.child(myRef.push().key ?: "blabla")
+                .setValue(User(auth.currentUser?.displayName, binding.edMessage.text.toString()))
         }
         onChangeListener(myRef)
+        initRcView()
+    }
 
+    private fun initRcView() = with(binding) {
+        adapter = UserAdapter()
+        rcView.layoutManager = LinearLayoutManager(this@MainActivity)
+        rcView.adapter = adapter
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.sign_out) {
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun onChangeListener(dRef: DatabaseReference) {
         dRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                binding.apply {
-                    rcView.append("\n")
-                    rcView.append("Sergey: ${snapshot.value.toString()}")
+                val list = ArrayList<User>()
+                for (s in snapshot.children) {
+                    val user = s.getValue(User::class.java)
+                    if(user!=null)list.add(user)
                 }
+                adapter.submitList(list)
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -51,9 +78,20 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun setUpActionBar() {
+        val ab = supportActionBar
+        Thread {
+            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val dIcon = BitmapDrawable(resources, bMap)
+            runOnUiThread {
+                ab?.setDisplayHomeAsUpEnabled(true)
+                ab?.setHomeAsUpIndicator(dIcon)
+                ab?.title = auth.currentUser?.displayName
+            }
 
+        }.start()
 
-
+    }
 
 
 }
---
+++ app/src/main/java/com/example/messengerx/User.kt
@@ -0,0 +1,6 @@
+package com.example.messengerx
+
+data class User(
+    val name:String?=null,
+    val message:String?=null
+)
